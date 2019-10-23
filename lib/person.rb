dfbafb17d8c844ce8a79abce52c1787fa8bf8dc1
Richard Lee <richlee81192@gmail.com>
2019-10-23 22:03:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,7 @@
 class Person
   attr_accessor :name, :hair_color, :birthday
   
-  def initialize(attribute)
-  attribute.each do |key, value| self.send((#{key}=), value)
-end
-end
+ def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
